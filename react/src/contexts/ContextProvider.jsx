5a51487045e4dfc9f55803c2cc6874036a2f3ca8
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-04-29 11:31:28 +0500

    react-laravel-project

---
+++ react/src/contexts/ContextProvider.jsx
@@ -0,0 +1,30 @@
+import{Children, createContext} from 'react';
+
+
+
+const StateContext = createContext({
+    currentUser : null,
+    token : null,
+});
+
+
+export const ContextProvider = ({children}) => {
+
+    const [user, serUser] = useState({});
+    const [token,  _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+    const setToken =  (token)=>{
+        _setToken(token);
+        if(token){
+            localStorage.setItem('ACCESS_TOKEN', token)
+        }  
+        else{
+            localStorage.removeItem('ACCESS_TOKEN')
+        }
+    return  (
+        <StateContext.Provider value ={{
+
+        }}>
+
+        </StateContext.Provider>
+    )
+}
