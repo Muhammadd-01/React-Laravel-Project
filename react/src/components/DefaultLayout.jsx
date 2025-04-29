738d40475c99e69d6dd221771d922df12d49553f
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-04-29 16:04:06 +0500

    react-laravel-project

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider'
 
 export default function DefaultLayout() {
+
+  const {user, token } = useStateContext()
+  if(!token){
+    return < Navigate to="/Login" />
+  }
   return (
     <div>
       <Outlet />
