31001c6770e64eb0f32cb492d4ccf0afb65dbc95
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-04-29 16:10:35 +0500

    react-laravel-project

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -3,9 +3,11 @@ import { Outlet } from 'react-router-dom'
 import { useStateContext } from '../contexts/ContextProvider'
 export default function GuestLayout() {
   const {token} = useStateContext()
+  
   if(!token){
     return <Navigate to="/" />
   }
+
   return (
     <div>
       <Outlet />
