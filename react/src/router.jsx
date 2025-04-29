4d688174e291eedc78e6e029d165a668be44fd38
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-04-29 10:41:36 +0500

    react-laravel-project

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -3,21 +3,40 @@ import Login from './views/Login';
 import Signup from './views/Signup';
 import Users from './views/Users';
 import NotFound from './views/NotFound';
+import GuestLayout from './components/GuestLayout';
+import GuestLayout from './components/DefaultLayout';
 
 const router = createBrowserRouter( [
-      {
-        path : "/login",
-        element : <Login/>,
-      },
-      {
-        path : "/signup",
-        element : <Signup/>,
 
-      },
-      {
-        path : "/Users",
-        element : <Users/>,
-      },
+    {
+        path : "/",
+        element : <DefaultLayout />,
+        children : [
+            {
+                path : "/Users",
+                element : <Users/>,
+              },
+        ]
+    },
+    {
+        path : "/",
+        elemnt : <GuestLayout/>,
+        children : [
+            {
+            path : "/login",
+            element : <Login/>,
+          },
+          {
+            path : "/signup",
+            element : <Signup/>,
+    
+          },
+         
+        ]
+      
+    },
+      
+     
       {
         path : "*",
         element : <NotFound/>,
